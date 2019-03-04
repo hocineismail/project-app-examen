01dfb28865b3c38851a25daa6d82fa09f2fb680c
hocineimail <hocineimail@github.com>
2019-03-04 17:12:11 +0100

    fix conflit

--- app.js
+++ app.js
@@ -10,14 +10,9 @@ var session = require("express-session");
 var flash = require("connect-flash"); 
 var passport = require("passport");
 
-<<<<<<< HEAD
 
-mongoose.connect("mongodb://localhost:27017/projectexams");
-=======
 mongoose.connect("mongodb://localhost:27017/projectfina");
->>>>>>> project
 
-=======
 var express = require('express')
 var app = express()
 var mongoose = require('mongoose')
