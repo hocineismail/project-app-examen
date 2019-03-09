3ab4b84a8924b05fdb984a6a8f1ce40bf9514c03
hocineimail <hocineismail75@gmail.com>
2019-03-09 13:14:04 +0100

    Add attribut IsValid model exan

--- models/exam.js
+++ models/exam.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema
 var examSchema = Schema({
  Exam: { type: String, required: true },
  Etat:  { type: Boolean },
+IsValid:  { type: Boolean, default: false },
  EtatFinal:  { type: Boolean, default: false },
  NumberOfExam: { type: Number, required: true },
  IsOfficial: { type: Boolean, required: true },
